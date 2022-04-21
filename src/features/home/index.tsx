4b4ee92d9ef2bae5a3609d54203c09ff54b1b49e
SeonHakJi <jshag90@naver.com>
2022-04-21 23:24:49 +0900

    [Add] 네이버 개발자 센터 앱등록 후 kConsumerKey, kConsumerSecret 변경 완료

--- src/features/home/index.tsx
+++ src/features/home/index.tsx
@@ -1,13 +1,74 @@
 import React from 'react';
-import {View} from 'native-base';
-import HamburgerBtn from '../../common/components/HamburgerBtn';
-// import Icon from 'react-native-vector-icons/FontAwesome';
+import {Alert, SafeAreaView, StyleSheet, Button, Platform} from 'react-native';
+import {NaverLogin, getProfile} from '@react-native-seoul/naver-login';
+
+const iosKeys = {
+  kConsumerKey: 'VC5CPfjRigclJV_TFACU',
+  kConsumerSecret: 'f7tLFw0AHn',
+  kServiceAppName: '테스트앱(iOS)',
+  kServiceAppUrlScheme: 'testapp', // only for iOS
+};
+
+const androidKeys = {
+  kConsumerKey: '0ItHds0lfc5nywExNGI0',
+  kConsumerSecret: 'LilXdv0hPO',
+  kServiceAppName: 'anytime',
+};
+
+const initials = Platform.OS === 'ios' ? iosKeys : androidKeys;
 
 function Home() {
+  const [naverToken, setNaverToken] = React.useState(null);
+
+  const naverLogin = props => {
+    return new Promise((resolve, reject) => {
+      NaverLogin.login(props, (err, token) => {
+        console.log(`\n\n  Token is fetched  :: ${token} \n\n`);
+        setNaverToken(token);
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(token);
+      });
+    });
+  };
+
+  const naverLogout = () => {
+    NaverLogin.logout();
+    setNaverToken('');
+  };
+
+  const getUserProfile = async () => {
+    const profileResult = await getProfile(naverToken.accessToken);
+    if (profileResult.resultcode === '024') {
+      Alert.alert('로그인 실패', profileResult.message);
+      return;
+    }
+    console.log('profileResult', profileResult);
+  };
+
   return (
-    <View mt={4} ml={4}>
-      <HamburgerBtn />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Button
+        title="네이버 아이디로 로그인하기"
+        onPress={() => naverLogin(initials)}
+      />
+      {!!naverToken && <Button title="로그아웃하기" onPress={naverLogout} />}
+
+      {!!naverToken && (
+        <Button title="회원정보 가져오기" onPress={getUserProfile} />
+      )}
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+  },
+});
+
 export default Home;
