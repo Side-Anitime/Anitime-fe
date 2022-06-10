d6da5bb677046539c903eb220f8a82fb72c8327d
SooYoung <kang.sooyoung@hotmail.com>
2022-06-10 15:48:17 +0900

    1. [Style] 반려동물 화면 스타일 컴포넌트 간소화 2. [Add] 문자열 날짜 형태 변환 유틸 추가

--- src/common/utils/DateUtils.ts
+++ src/common/utils/DateUtils.ts
@@ -6,3 +6,10 @@ export const formatDate = (date: Date, format?: string): string => {
   }
   return moment(date).format('YYYY-MM-DD');
 };
+
+export const formatDateString = (date: string, format?: string): string => {
+  if (format) {
+    return moment(date).format(format);
+  }
+  return moment(date).format('YYYY-MM-DD');
+};
