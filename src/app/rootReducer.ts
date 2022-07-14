a6a36a4dda0dcda90cb9f2908c9d4358a6a309a1
SooYoung <kang.sooyoung@hotmail.com>
2022-07-14 14:37:22 +0900

    [Add] 공용 액션 버튼 추가/ 로딩 관리

--- src/app/rootReducer.ts
+++ src/app/rootReducer.ts
@@ -2,10 +2,12 @@ import {combineReducers} from 'redux';
 import petInfoSlice from '../features/mypet/petInfoSlice';
 import calendarSlice from '../features/calendar/calendarSlice';
 import bottomSheetPetSlice from '../common/components/BottomSheetPet/bottomSheetPetSlice';
+import loadingSlice from '../features/loading/loadingSlice';
 
 const rootReducer = combineReducers({
   calendar: calendarSlice.reducer,
   petInfo: petInfoSlice.reducer,
+  loading: loadingSlice.reducer,
   bottomSheetPet: bottomSheetPetSlice.reducer,
 });
 
